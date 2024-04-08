cb1b2d9f5e6ecfdd154655371af402ec7fd32d4c
BrainyYawson <acourageyawson369@gmail.com>
2024-04-08 02:13:51 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './pages/homepage/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu-item/Menu-items.component.jsx
@@ -0,0 +1,16 @@
+import './menu-items.scss'
+
+// functional Component
+const MenuItem = ({ title, imageUrl, size }) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} />
+            <div className="content">
+                <h1 className="title">{ title.toUpperCase() }</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    );
+}
+ 
+export default MenuItem;
